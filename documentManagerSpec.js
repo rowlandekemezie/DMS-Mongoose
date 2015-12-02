5224c192085d282a3dd9e692f228393df56925dc
rowland <rowlandekemezie@gmail.com>
2015-12-02 04:19:39 +0100

    test(core): add test suites
    -move documentManagerSpec.js to the spec/
    -delete documentManagerSpec.js from root directory

--- documentManagerSpec.js
+++
@@ -1,9 +0,0 @@
-var dms = require('./documentManager');
-var seeds =require('./seeds');
-console.log(seeds);
-console.log(dms);
-describe("User", function(){
-  it("should be truthy", function(){
-      expect(2).toBe(2);
-  });
-});
